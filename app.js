f9b4a5fc112573241ee70eee40ea08e0a4d17702
tamueka <tamueka@hotmail.com>
2019-04-14 00:30:27 +0200

    agregamos ruta lang en app.js + gurdamos cookie con el nombre node-api + creamos archivo ruoutes/lang.js  + añadimos cambios a  los enlaces EN ES y lo hacemos dinamicos

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use('/apiv1/agentes', require('./routes/apiv1/agentes'));
  */
 app.use('/',        require('./routes/index'));
 app.use('/about',   require('./routes/about'));
+app.use('/lang',    require('./routes/lang'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
